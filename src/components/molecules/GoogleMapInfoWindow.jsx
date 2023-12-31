25e98a2466a6446806c08323ac8cde2e648761e4
Derek Oshita <derek@joinstatus.com>
2023-12-30 19:17:20 -0800

    removed container attribute to fix marker info windows.

--- src/components/molecules/GoogleMapInfoWindow.jsx
+++ src/components/molecules/GoogleMapInfoWindow.jsx
@@ -1,4 +1,5 @@
 import { InfoWindowF } from "@react-google-maps/api";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 
 export const GoogleMapInfoWindow = ({
@@ -17,10 +18,16 @@ export const GoogleMapInfoWindow = ({
       position={{ lat, lng }}
       onCloseClick={onClose}
     >
-      <div>
-        <a href={url} target="_blank" rel="noopener noreferrer" >{label}</a>
-        <p>{address}</p>
-        <p>{distanceInMinutes} mins away</p>
+      <div className="flex flex-col">
+        <a href={url} target="_blank" rel="noopener noreferrer" >
+          <section className="flex p-1">
+            <strong>{label}</strong><div className="ml-1"><FaExternalLinkAlt /></div>
+          </section>
+        </a>
+        <section className="p-1">
+          <p>{address}</p>
+          <p>{distanceInMinutes} mins away</p>
+        </section>
       </div>
     </InfoWindowF>
   );
